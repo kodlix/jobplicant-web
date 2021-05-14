feb652e5e9296ca1397c18f7d943ab0765489c8d
Johnson Iyida <kodili.johnson@gmail.com>
2021-05-14 06:56:45 +0100

    update server url

--- src/services/agent.service.js
+++ src/services/agent.service.js
@@ -6,6 +6,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 
 // export const API_ROOT = process.env.NODE_ENV === "development" ? (process.env.API_ROOT_LOCAL || 'http://localhost:8080') : process.env.API_ROOT_PROD;
 export const API_ROOT  = "https://jobplicant-api-pyfpg.ondigitalocean.app";
+console.log(API_ROOT);
 export const IMAGE_URL = API_ROOT + '/account/uploads/';
 
 let accessToken = null;
