8dd038a3a6b3ae5da24e69e31c2cb90e2b06dd3e
Johnson Iyida <kodili.johnson@gmail.com>
2021-05-14 07:26:11 +0100

    update environment

--- src/services/agent.service.js
+++ src/services/agent.service.js
@@ -4,9 +4,8 @@ import superagentPromise from 'superagent-promise';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-// export const API_ROOT = process.env.NODE_ENV === "development" ? (process.env.API_ROOT_LOCAL || 'http://localhost:8080') : process.env.API_ROOT_PROD;
-export const API_ROOT  = "https://jobplicant-api-pyfpg.ondigitalocean.app";
-console.log(API_ROOT);
+export const API_ROOT = process.env.NODE_ENV === "development" ? process.env.API_ROOT_LOCAL : "https://jobplicant-api-pyfpg.ondigitalocean.app";
+console.log('API_ROOT', API_ROOT);
 console.log("environmental variables", process.env);
 export const IMAGE_URL = API_ROOT + '/account/uploads/';
 
